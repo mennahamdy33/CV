79b763f2dbae17e21eb7da3c3311f23b84e63478
Marwa Yossef <marwaayosiif@yahoo.com>
2021-03-26 17:09:50 +0200

    Point3Modification

--- Task1/main.py
+++ Task1/main.py
@@ -14,12 +14,20 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         self.padded =[]
         self.R=0;
         self.C=0;
-        self.n=3 
+        self.n=3
         self.ui.actionExit.triggered.connect(exit)
         self.ui.actionInput.triggered.connect(self.getPicrures)
         self.ui.filter.clicked.connect(lambda:self.AvgFilter(self.padded,self.R,self.C,self.n))
-        self.ui.edge.clicked.connect(lambda:self.SobelFilter(self.padded,self.R,self.C,self.n))
-        
+        self.ui.edge.activated.connect(self.chooseEdge)
+
+    def chooseEdge(self):
+        if str(self.ui.edge.currentText())=="Edge In X":
+            maskX = [[-1,0,1],[-2,0,2],[-1,0,1]]
+            self.SobelFilter(self.padded,self.R,self.C,self.n,maskX)
+        if str(self.ui.edge.currentText())=="Edge In Y":
+            maskY = [[1,2,1],[0,0,0],[-1,-2,-1]]    
+            self.SobelFilter(self.padded,self.R,self.C,self.n,maskY)
+
     def AvgFilter(self,img,R,C,n):
         mask = np.ones((3,3),np.float32)/9
         newImage = np.zeros((R+n-1,C+n-1))
@@ -28,20 +36,21 @@ class ApplicationWindow(QtWidgets.QMainWindow):
                     newImage[i-1,j-1] = np.sum(np.multiply(mask,img[i:i+n,j:j+n]))
         cv2.imwrite("Filtered.png",newImage)
         self.ui.output1.setPixmap(QPixmap("G:/SBME/CV/Tasks/CV/Task1/Filtered.png"))
-    def SobelFilter(self,img,R,C,n):
-        maskX = [[-1,0,1],[-2,0,2],[-1,0,1]]
-        maskY = [[1,2,1],[0,0,0],[-1,-2,-1]]
+
+    def SobelFilter(self,img,R,C,n,mask):
         newImage = np.zeros((R+n-1,C+n-1))
         for i in range(1,R-2):
             for j in range(1,C-2):
-                    newImage[i-1,j-1] = np.sum(np.multiply(maskY,img[i:i+n,j:j+n]))
-        cv2.imwrite("Filtered.png",newImage)
-        self.ui.output1.setPixmap(QPixmap("G:/SBME/CV/Tasks/CV/Task1/Filtered.png"))    
+                    newImage[i-1,j-1] = np.sum(np.multiply(mask,img[i:i+n,j:j+n]))
+        cv2.imwrite("Edge.png",newImage)
+        self.ui.output1.setPixmap(QPixmap("G:/SBME/CV/Tasks/CV/Task1/Edge.png")) 
+
     def padding(self,img,n):
         self.R,self.C= img.shape
         imgAfterPadding = np.zeros((self.R+self.n-1,self.C+self.n-1))
         imgAfterPadding[1:1+self.R,1:1+self.C] = img.copy()
         return(imgAfterPadding)       
+        
     def getPicrures (self):
         path,extention = QtWidgets.QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "") 
         if path == "":
@@ -49,8 +58,6 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         else:
             image = cv2.imread(path,0)
             self.padded = self.padding(image,self.n)
-            print(path)
-            print(QPixmap(path))
             self.ui.Input1.setPixmap(QPixmap(path))
 
 def main():
