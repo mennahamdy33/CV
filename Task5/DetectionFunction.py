37e0208ed0dae1d087a0bd5409248c5b93d79a54
Marwa Yossef <marwaayosiif@yahoo.com>
2021-05-29 23:51:30 +0200

    Done with face detection

--- Task5/DetectionFunction.py
+++ Task5/DetectionFunction.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2 
 import os
 
+
 def detect_faces(test_image, scaleFactor = 1.1, minNeighbors = 1):
     image_copy = test_image.copy()
     haar_cascade_face = cv2.CascadeClassifier('data/haarcascades/haarcascade_frontalface_alt2.xml')
@@ -24,16 +25,16 @@ def detect_faces(test_image, scaleFactor = 1.1, minNeighbors = 1):
     return image_copy , len(faces_rect)
 
 
-directory = "training-originals"
-counter =0 
-for filename in os.listdir(directory):
-    if filename.endswith(".jpg") : 
-        path = os.path.join(directory, filename)
-        test_image2 = cv2.imread(path)
-        faces , count= detect_faces( test_image2)
-        if (count == 1):
-            counter = counter +1
-        cv2.imwrite("./test/{}".format(filename),faces)
-    else:
-        continue
+# directory = "training-originals"
+# counter =0 
+# for filename in os.listdir(directory):
+#     if filename.endswith(".jpg") : 
+#         path = os.path.join(directory, filename)
+#         test_image2 = cv2.imread(path)
+#         faces , count= detect_faces( test_image2)
+#         if (count == 1):
+#             counter = counter +1
+#         cv2.imwrite("./test/{}".format(filename),faces)
+#     else:
+#         continue
 # print(counter)
